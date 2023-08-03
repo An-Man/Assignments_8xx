659f0d1b304983aeae15d526977a1e261f148ba4
An-Man <anna.pikkarainen@gmail.com>
2023-08-03 16:34:53 +0300

    wip

---
+++ a842/memerror1.h
@@ -0,0 +1,34 @@
+#ifndef ERROR1_H
+#define ERROR1_H
+
+#include <vector>
+#include <iostream>
+
+struct object_data
+{
+    std::string name;
+};
+
+static std::vector<object_data> object_storage {1};
+
+class large_object
+{
+    public:
+        large_object();
+        ~large_object();
+        large_object(const large_object &) = default;   // default the copy
+                                                        //constructor, since
+                                                  // the class is basically just
+                                                  // the handle anyways
+        
+        private :
+        // int is often used as a handle, so negative values an be used to denote
+        // errors. std::ssize_t is another often seen choice, but we aren't going
+        // to create more than INT_MAX elements in our container here.
+        int handle;
+};
+
+
+
+
+#endif
